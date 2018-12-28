3f24e00ed7a5fa4601dfeb9200a6c2b0008786a5
alexp25 <predescu.alex93@gmail.com>
2018-12-28 22:49:37 +0200

    update plot tool

--- results/plot_results.py
+++ results/plot_results.py
@@ -18,8 +18,9 @@ print(headers)
 
 class ContainerElement:
     label = "",
-    key = "",
+    key = None,
     color = "",
+    plot = True,
     marker = None,
     index = -1,
     data = []
@@ -34,33 +35,64 @@ m = [
     {
         'label': 'y',
         'key': 'calibratedHeading',
+        'disp': False,
         'color': 'blue'
     },
     {
         'label': 'yp',
         'key': 'gyroHeading',
+        'disp': True,
         'marker': '+',
         'color': 'red'
     },
     {
         'label': 'yref',
         'key': 'compassHeading',
+        'disp': True,
+        'color': 'green'
+    },
+    {
+        'label': 'diff',
+        'disp': False,
         'color': 'green'
     }
 ]
+
+
+def get_mc_by_key(mc, key):
+    for m in mc:
+        if m.key == key:
+            return m
+    return None
+
+def get_mc_by_label(mc, key):
+    for m in mc:
+        if m.label == key:
+            return m
+    return None
 
 mc = []
 
 for (index, e) in enumerate(m):
     new_elem = ContainerElement()
-    new_elem.key = e['key']
+
+    if 'key' in e:
+        new_elem.key = e['key']
+
     new_elem.label = e['label']
     new_elem.color = e['color']
+
+    if 'disp' in e:
+        new_elem.plot = e['disp']
+    else:
+        new_elem.plot = False
+
     if 'marker' in e:
         new_elem.marker = e['marker']
     else:
         new_elem.marker = None
         # print(e['marker'])
+    new_elem.index = -1
     for (index2, h) in enumerate(headers):
         if h == new_elem.key:
             new_elem.index = index2
@@ -90,6 +122,14 @@ x1 = np.array(x)
 x1 = (x1 - np.min(x1))/1000
 x = list(x1)
 
+
+diff = get_mc_by_label(mc, 'diff')
+d1 = get_mc_by_key(mc, 'compassHeading')
+d2 = get_mc_by_key(mc, 'gyroHeading')
+d1 = np.array(d1.data)
+d2 = np.array(d2.data)
+diff.data = list(d2 - d1)
+
 samples = None
 # samples = 1000
 
@@ -105,10 +145,11 @@ for (index, e) in enumerate(mc):
             e.data = e.data[0:samples]
 
         # print(e.key)
-        if e.marker is not None:
-            plt.plot(x, e.data, label=e.label, marker=e.marker)
-        else:
-            plt.plot(x, e.data, label=e.label)
+        if e.plot:
+            if e.marker is not None:
+                plt.plot(x, e.data, label=e.label, marker=e.marker)
+            else:
+                plt.plot(x, e.data, label=e.label)
 
 plt.xlabel('time (s)')
 plt.ylabel('y')
